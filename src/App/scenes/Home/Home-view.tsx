2770fde9e0168c8c854db0b2b5fd1e0064897583
Adrián García Diéguez <adrigar@Adrians-MacBook-Pro.local>
2019-12-19 20:47:19 +0100

    Skeleton config

--- src/App/scenes/Home/Home-view.tsx
+++ src/App/scenes/Home/Home-view.tsx
@@ -1,31 +1,32 @@
-import * as React from 'react';
+import * as React from "react";
 
-import './Home.scss'
+import "./Home.scss";
 
 export const HomeView = (props: any) => {
-    return (props.data
-        &&
-        <div className="home">
-            <div className="home__title">
-                <div className="main-container">
-                    <h1> {props.data.title} </h1>
-                    <div className="separator"></div>
-                    <h3> {props.data.subtitle}</h3>
-                </div>
-            </div>
-
-            <div className="home__team">
-                <div className="main-container">
-                    <h3>{props.data.team_subtitle}</h3>
-                </div>
-            </div>
-
-            <div className="home__bottom">
-                <div className="main-container">
-                    <h1>{props.data.bottom__title}</h1>
-                    <p>{props.data.bottom__subtitle}</p>
-                </div>
-            </div>
+  return (
+    <div className="home">
+      <div className="home__title">
+        <div className="main-container">
+          <h1 className="skeleton">{props.data && props.data.title}</h1>
+          <div className="separator"></div>
+          <h3 className="skeleton">{props.data && props.data.subtitle}</h3>
         </div>
-    )
-}
+      </div>
+
+      <div className="home__team">
+        <div className="main-container">
+          <h3 className="skeleton">{props.data && props.data.team_subtitle}</h3>
+        </div>
+      </div>
+
+      <div className="home__bottom">
+        <div className="main-container">
+          <h1 className="skeleton">{props.data && props.data.bottom__title}</h1>
+          <p className="skeleton">
+            {props.data && props.data.bottom__subtitle}
+          </p>
+        </div>
+      </div>
+    </div>
+  );
+};
--- src/App/scenes/Projects/Projects-view.tsx
+++ src/App/scenes/Projects/Projects-view.tsx
@@ -1,39 +1,43 @@
-import * as React from 'react';
-import Grid from '@material-ui/core/Grid';
+import * as React from "react";
+import Grid from "@material-ui/core/Grid";
 
-import './Projects.scss'
-import { Card } from './components/Card';
+import "./Projects.scss";
+import { Card } from "./components/Card";
 
 export const ProjectsView = (props: any) => {
-    props = {}
-    return (
-        // props.data &&
-            (<div className="projects">
-                <div className="background">
-                    <div className="projects__title">
-                        <div className="main-container">
-                            <p>{(props.data && props.data.title)}</p>
-                        </div>
-                    </div>
-                    <div className="projects__subtitle">
-                        <div className="main-container">
-                            <p>{(props.data && props.data.subtitle)}</p>
-                        </div>
-                    </div>
-                </div>
-                <div className="projects__cards">
-                    <div className="main-container">
-                        <Grid container spacing={5}>
-                            {
-                                props.data && props.data.projects.map((project: any, index: number) => (
-                                    <Grid item xs={12} sm={6} md={4} className="container" key={index}>
-                                        <Card {...project} />
-                                    </Grid>
-                                ))
-                            }
-                        </Grid>
-                    </div>
-                </div>
-            </div>)
-    )
-}
+  return (
+    <div className="projects">
+      <div className="background">
+        <div className="projects__title">
+          <div className="main-container">
+            <p className="skeleton">{props.data && props.data.title}</p>
+          </div>
+        </div>
+        <div className="projects__subtitle">
+          <div className="main-container">
+            <p className="skeleton">{props.data && props.data.subtitle}</p>
+          </div>
+        </div>
+      </div>
+      <div className="projects__cards">
+        <div className="main-container">
+          <Grid container spacing={5}>
+            {props.data &&
+              props.data.projects.map((project: any, index: number) => (
+                <Grid
+                  item
+                  xs={12}
+                  sm={6}
+                  md={4}
+                  className="container"
+                  key={index}
+                >
+                  <Card {...project} />
+                </Grid>
+              ))}
+          </Grid>
+        </div>
+      </div>
+    </div>
+  );
+};
